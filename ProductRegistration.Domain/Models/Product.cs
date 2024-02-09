c29bcfe2b7a3e7d4829340250b9a07d9c529527f
wadsonsm <wadsonsm@hotmail.com>
2024-02-09 18:00:11 -0300

    Add project files.

---
+++ ProductRegistration.Domain/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductRegistration.Domain.Models
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductCost { get; set; }
+        public int Stock { get; set; }
+    }
+}
