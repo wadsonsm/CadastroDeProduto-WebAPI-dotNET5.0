289c51a6ceb2ab1d398157702fea6b311fcd83f1
wadsonsm <wadsonsm@hotmail.com>
2024-02-10 13:52:32 -0300

    conclusão operações Crud

--- ProductRegistration/Controllers/ProductController.cs
+++ ProductRegistration/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProductRegistration.API.DataContext;
 using ProductRegistration.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -27,10 +28,59 @@ namespace ProductRegistration.API.Controllers
 
         [HttpGet]
         [Route("ProductDetails")]
-        public async Task<ActionResult<Product>> Get(string id)
+        public async Task<ActionResult<Product>> Get(Guid id)
         {
             var product = await _context.Products.FindAsync(id);
             return Ok(product);
+        }
+
+        [HttpPost]
+        [Route("CreateRecord")]
+        public async Task<ActionResult<Product>> POST(Product product)
+        {
+            if (product == null)
+                return BadRequest();
+            
+            this._context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("UpdateProduct")]
+        public async Task<ActionResult<Product>> Update(Guid id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            var productData = await _context.Products.FindAsync(id);
+            if (productData == null)            
+                return NotFound();
+            
+            if(!ModelState.IsValid)
+                return BadRequest();
+
+            _context.Entry(productData).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("DeleteProduct")]
+        public async Task<ActionResult<Product>> Delete(Guid id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+                return NotFound();
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return Ok();            
         }
     }
 }
